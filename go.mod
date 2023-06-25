abf3b1074a0f2457bd7551a0f6292c863dd8e942
EnbinYang <enbinyang1999@163.com>
2023-06-25 14:01:06 +0800

    write both Redis and Bloom filter

--- go.mod
+++ go.mod
@@ -10,6 +10,11 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/huandu/go-sqlbuilder v1.21.0 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/willf/bitset v1.1.11 // indirect
+	github.com/willf/bloom v2.0.3+incompatible // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
@@ -17,4 +22,6 @@ require (
 	google.golang.org/grpc v1.55.0 // indirect
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gorm.io/driver/mysql v1.5.1 // indirect
+	gorm.io/gorm v1.25.1 // indirect
 )
---
+++ redis/bloom_filter/bloom_filter.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/willf/bloom"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	ctx := context.Background()
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "1",
+		DB:       0,
+	})
+	defer redisClient.Close()
+
+	conn := "debian-sys-maint:P7g9fAYfTM4YhtoC@tcp(localhost:3306)/webserver"
+	db, err := gorm.Open(mysql.Open(conn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to MySQL:", err)
+	}
+
+	// create bloom filter
+	bf := bloom.NewWithEstimates(1000000, 0.01)
+
+	// select data from MySQL
+	rows, err := db.Raw("SELECT username, password FROM user").Rows()
+	if err != nil {
+		log.Fatal("Error executing MySQL query:", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var uname string
+		var password string
+		err := rows.Scan(&uname, &password)
+		if err != nil {
+			log.Fatal("Error scanning MySQL row:", err)
+			continue
+		}
+
+		// insert data into Redis
+		err = redisClient.Set(ctx, fmt.Sprintf("username:%s", uname), password, 0).Err()
+		if err != nil {
+			log.Fatal("Error setting Redis:", err)
+			continue
+		}
+
+		// insert data into bloom filter
+		bf.Add([]byte(fmt.Sprintf("%s", uname)))
+	}
+
+	queryName := "enbin"
+	if bf.Test([]byte(fmt.Sprintf("%s", queryName))) {
+		// bloom filter hit
+		password, err := redisClient.Get(ctx, fmt.Sprintf("username:%s", queryName)).Result()
+		if err != nil {
+			log.Fatal("Error reading from Redis:", err)
+		} else {
+			log.Println("Data found in Redis:", password)
+		}
+	} else {
+		// query data from MySQL
+		var password string
+		err := db.Raw("SELECT password FROM user WHERE username = ?", queryName).Scan(&password).Error
+		if err != nil {
+			log.Fatal("Error querying MySQL:", err)
+		} else {
+			log.Println("Data found in MySQL:", password)
+		}
+	}
+
+	// close MySQL connection
+	dbSQL, err := db.DB()
+	if err != nil {
+		log.Fatal("Error closing MySQL connection:", err)
+	}
+	dbSQL.Close()
+}
